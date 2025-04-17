287fdcf4ad3dabd8f2d96804d58f8fe7de634bad
Zhalslar <zsl2936169201@gmail.com>
2025-04-17 20:19:12 +0800

    去除无关功能

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
+
+<div align="center">
+
+![:name](https://count.getloli.com/@astrbot_plugin_QQProfile?name=astrbot_plugin_QQProfile&theme=minecraft&padding=6&offset=0&align=top&scale=1&pixelated=1&darkmode=auto)
+
 # astrbot_plugin_QQProfile
 
-## 介绍
+_✨ [astrbot](https://github.com/AstrBotDevs/AstrBot) QQ资料配置插件 ✨_  
 
-利用napcat提供的接口配置QQ机器人的头像、昵称、签名、状态，查看\删除好友、群聊，查看点赞列表、清除未读等等
+[![License](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+[![Python 3.10+](https://img.shields.io/badge/Python-3.10%2B-blue.svg)](https://www.python.org/)
+[![AstrBot](https://img.shields.io/badge/AstrBot-3.4%2B-orange.svg)](https://github.com/Soulter/AstrBot)
+[![GitHub](https://img.shields.io/badge/作者-Zhalslar-blue)](https://github.com/Zhalslar)
+
+</div>
+
+## 🤝 介绍
+
+本插件利用Onebot协议接口配置QQ机器人的头像、昵称、签名、状态，查看\删除好友、群聊，查看点赞列表、清除未读等等
 
 ## 📦 安装
+
+- 可以直接在astrbot的插件市场搜索astrbot_plugin_QQProfile，点击安装即可  
 
 ```bash
 # 克隆仓库到插件目录
@@ -14,31 +30,33 @@ git clone https://github.com/Zhalslar/astrbot_plugin_QQProfile
 # 控制台重启AstrBot
 ```
 
-
 ## ⚙️ 配置
- 
+
 请在astrbot面板配置，插件管理 -> astrbot_plugin_QQProfile -> 操作 -> 插件配置
 
-
 ## 🐔 使用说明
+
 ### 指令表
+
 |     指令     |             说明              |
 |:----------:|:---------------------------:|
 | [引用图片]设置头像 |        将引用的这张图片设置为头像        |
 |  设置昵称 XXX  |        将Bot的昵称改为XXX         |
 |  设置签名 XXX  | 将Bot的签名改为XXX，并同步空间（可在QQ里关掉） |
 |  设置状态 XXX  |     设置Bot的在线状态（如“我的电量”）     |
-|    点赞列表    |          查看谁赞了Bot           |
-|    清除未读    |        将所有未读消息标记为已读         |
+
+### 示例图
+
+![eb62d8cb2e51b1caadab31f746ab923](https://github.com/user-attachments/assets/5dff2926-83c1-4aa8-8b2a-da3bf3baef36)
+
+## 👥 贡献指南
+
+- 🌟 Star 这个项目！（点右上角的星星，感谢支持！）
+- 🐛 提交 Issue 报告问题
+- 💡 提出新功能建议
+- 🔧 提交 Pull Request 改进代码
 
 ## 📌 注意事项
-目前仅测试了napcat，其他Onebot协议端可能也能用，但可能部分接口用不了
 
-
-## TODO
-- 提供更多配置项
-- 测试更多环境下的部署
-
-## 📜 开源协议
-本项目采用 [MIT License](LICENSE)
-
+- 本插件目前仅测试了napcat协议端，其他协议端可能会存在一些不兼容问题（以具体情况为准）
+- 想第一时间得到反馈的可以来作者的插件反馈群（QQ群）：460973561
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-
-
 from astrbot.api.event import filter
 from astrbot.api.star import Context, Star, register
 import astrbot.api.message_components as Comp
 from astrbot.core.platform import AstrMessageEvent
-from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
+from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+    AiocqhttpMessageEvent,
+)
 from astrbot.core.star.filter.permission import PermissionType
 from .status import status_mapping
 
-@register("astrbot_plugin_QQProfile", "Zhalslar", "[仅aiocqhttp]QQ机器人信息配置插件", "1.0.0")
+
+@register(
+    "astrbot_plugin_QQProfile",
+    "Zhalslar",
+    "[仅aiocqhttp] 配置bot的头像、昵称、签名、状态、机型等QQ资料",
+    "1.0.0",
+)
 class QQProfilePlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -16,18 +22,18 @@ class QQProfilePlugin(Star):
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("设置头像", desc="将引用的图片设置为头像")
     async def set_avatar(self, event: AstrMessageEvent):
-
         messages = event.get_messages()
         img_url = None
         for seg in messages:
             if isinstance(seg, Comp.Image):
-                img_url= seg.url
+                img_url = seg.url
                 break
             elif isinstance(seg, Comp.Reply):
-                for reply_seg in seg.chain:
-                    if isinstance(reply_seg, Comp.Image):
-                        img_url = reply_seg.url
-                        break
+                if seg.chain:
+                    for reply_seg in seg.chain:
+                        if isinstance(reply_seg, Comp.Image):
+                            img_url = reply_seg.url
+                            break
 
         if img_url:
             assert isinstance(event, AiocqhttpMessageEvent)
@@ -35,133 +41,40 @@ class QQProfilePlugin(Star):
             await client.set_qq_avatar(file=img_url)
             yield event.plain_result("我头像更新啦>v<")
         else:
-            yield event.plain_result(f"找不到图片")
-
+            yield event.plain_result("找不到图片")
 
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("设置昵称", desc="设置Bot的昵称")
-    async def set_nickname(self, event: AiocqhttpMessageEvent, nickname:str=None):
+    async def set_nickname(self, event: AiocqhttpMessageEvent, nickname: str|None = None):
+        if not nickname:
+            yield event.plain_result("没提供新昵称呢")
+            return
         client = event.bot
         await client.set_qq_profile(nickname=nickname)
         yield event.plain_result(f"我昵称已改为【{nickname}】")
 
-
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("设置签名", desc="设置Bot的签名，并同步空间（可在QQ里关掉）")
-    async def set_longnick(self, event: AiocqhttpMessageEvent, longnick:str=None):
+    async def set_longnick(self, event: AiocqhttpMessageEvent, longnick: str|None = None):
+        if not longnick:
+            yield event.plain_result("没提供新签名呢")
+            return
         client = event.bot
         await client.set_self_longnick(longNick=longnick)
         yield event.plain_result(f"我签名已更新：{longnick}")
 
-
     @filter.permission_type(PermissionType.ADMIN)
     @filter.command("设置状态", desc="设置Bot的在线状态")
-    async def set_status(self, event: AiocqhttpMessageEvent, status_name:str=None):
+    async def set_status(self, event: AiocqhttpMessageEvent, status_name: str|None = None):
+        if not status_name:
+            yield event.plain_result("没提供新状态呢")
+            return
         client = event.bot
         params = status_mapping.get(status_name, None)
         if not params:
             yield event.plain_result(f"状态【{status_name}】暂未支持")
             return
         await client.set_online_status(
-            status=params[0],
-            ext_status=params[1],
-            battery_status=0
+            status=params[0], ext_status=params[1], battery_status=0
         )
         yield event.plain_result(f"我状态已更新为【{status_name}】")
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("点赞列表", desc="查看谁赞了Bot")
-    async def get_profile_like(self, event: AiocqhttpMessageEvent):
-        """获取自身点赞列表"""
-        client = event.bot
-        data = await client.get_profile_like()
-        reply = ""
-        user_infos = data.get('favoriteInfo', {}).get('userInfos', [])
-        for user in user_infos:
-            if ('nick' in user and user['nick'] and
-                    'count' in user and user['count'] > 0):
-                reply += f"\n【{user['nick']}】赞了我{user['count']}次"
-        if not reply:
-            reply = "暂无有效的点赞信息"
-        url = await self.text_to_image(reply)
-        yield event.image_result(url)
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("清除未读", desc="将所有未读消息标记为已读")
-    async def mark_all_as_read(self, event: AiocqhttpMessageEvent):
-        client = event.bot
-        await client._mark_all_as_read()
-        yield event.plain_result(f"已将所有未读消息标记为已读")
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("群列表")
-    async def show_groups_info(self, event: AiocqhttpMessageEvent):
-        """查看加入的所有群聊信息"""
-        client = event.bot
-        group_list = await client.get_group_list()
-        group_info = "\n".join(f"{g['group_id']}: {g['group_name']}" for g in group_list)
-        info = f"【群列表】共加入{len(group_list)}个群：\n{group_info}"
-        yield event.plain_result(info)
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("好友列表")
-    async def show_friends_info(self, event: AiocqhttpMessageEvent):
-        """查看所有好友信息"""
-        client = event.bot
-        friend_list = await client.get_friend_list()
-        friend_info = "\n".join(f"{f['user_id']}: {f['nickname']}" for f in friend_list)
-        info = f"【好友列表】共{len(friend_list)}位好友：\n{friend_info}"
-        yield event.plain_result(info)
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("退群")
-    async def set_group_leave(self, event: AiocqhttpMessageEvent, group_id:int=None):
-        """查看所有好友信息"""
-        if not group_id:
-            yield event.plain_result("要指明退哪个群哟~")
-            return
-
-        client = event.bot
-        group_list = await client.get_group_list()
-
-        group_ids = [int(group['group_id']) for group in group_list]
-        if group_id not in group_ids:
-            yield event.plain_result("我没加有这个群")
-            return
-
-        await client.set_group_leave(group_id=group_id)
-        yield event.plain_result(f"已退出群聊：{group_id}")
-
-
-    @filter.permission_type(PermissionType.ADMIN)
-    @filter.command("删了",alias={'删除好友'})
-    async def delete_friend(self, event: AiocqhttpMessageEvent, target_id:int=None):
-        """查看所有好友信息"""
-        chain = event.get_messages()
-        target_id: int = target_id or next(int(seg.qq) for seg in chain if (isinstance(seg, Comp.At)))
-
-        client = event.bot
-        friend_list = await client.get_friend_list()
-
-        friend_ids = [int(friend['user_id']) for friend in friend_list]
-        if target_id not in friend_ids:
-            yield event.plain_result("我没加有这个人")
-            return
-
-        await client.delete_friend(group_id=target_id)
-        yield event.plain_result(f"已删除好友：{target_id}")
-
-
-
-
-
-
-
-
-
-
